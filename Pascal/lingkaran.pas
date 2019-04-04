c1ad001815a350a8c523bfb6a204c1469e717179
ozer0532 <ozer0532@users.noreply.github.com>
2019-04-04 20:12:35 +0700

    Commit

---
+++ Pascal/lingkaran.pas
@@ -0,0 +1,25 @@
+(* Chokyi Ozer / 16518269 / Minggu 12 DasPro *)
+Program lingkaran;
+(* Input: jari-jari *)
+(* Output: luas lingkaran *)
+
+(* Kamus *)
+const
+    pi : real = 3.1415;
+
+var
+    r : integer;
+
+(* Algoritma *)
+begin
+    readln(r);
+
+    if ( r > 0 ) then
+    begin
+        writeln((pi * r * r):0:2);
+    end
+    else (* r <= 0 *)
+    begin
+        writeln('Jari-jari harus > 0');
+    end;
+end.
